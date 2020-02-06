5d176db466d21b3946bc2513e920398824babd67
Alan Dodge <alan.dodge@gov.bc.ca>
2020-02-06 13:31:01 -0800

    Removed most unused imports

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import './App.scss';
 import Header from './components/header/Header';
-import Breadcrumbs from './components/breadcrumbs/Breadcrumbs';
 import Body from './components/body/Body';
 import Footer from './components/footer/Footer';
 
--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,21 +1,8 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
 import './Body.scss';
 import Menu from '../menu/Menu';
 import Content from '../content/Content';
-
-const menuList = [
-  {
-    name: 'Home',
-    url: '/'
-  },
-  {
-    name: 'Gov',
-    url: 'gov.bc.ca'
-  }
-]
-
 
 export default function Body(props) {
 

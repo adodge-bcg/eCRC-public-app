a77a852e4d69458c97fe5ae030deeb9847146505
Alan Dodge <alan.dodge@gov.bc.ca>
2020-02-06 08:27:53 -0800

    Stubbed out component returns for ToS, Consent and Application

---
+++ src/components/content/applicationform/AplicationForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './ApplicationForm.scss';
+
+export default function ApplicationForm(props) {
+
+  return (
+    <div>
+      This is an application form.
+    </div>
+  )
+}
---
+++ src/components/content/termsofservice/TermsofService.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './TermsofService.scss';
+
+export default function TermsofService(props) {
+
+  return (
+    <div>
+      This is a Terms of Service form.
+    </div>
+  )
+}

0450fdcf922de37a14f7299d4a92220aa830b6b9
Alan Dodge <alan.dodge@gov.bc.ca>
2020-02-06 09:00:19 -0800

    Added custom hook and reducer for storing applicant info

---
+++ src/hooks/useApplicantData.js
@@ -0,0 +1,25 @@
+import useReducer from 'react';
+
+import reducer, {SET_ORG, SET_APPLICANT, SET_CONSENT} from '../reducers/reduceApplicantData';
+
+export default function useApplicantData() {
+  const [state, dispatch] = useReducer(reducer, {
+    org: {},
+    applicant: {},
+    consent: false
+  });
+
+  const setOrg = (org) => {
+    dispatch({type: SET_ORG, value: org});
+  };
+
+  const setApplicant = (applicant) => {
+    dispatch({type: SET_APPLICANT, value: applicant});
+  };
+
+  const setConsent = (consent) => {
+    dispatch({type: SET_CONSENT, value: consent});
+  };
+
+  return {state, setOrg, setApplicant, setConsent};
+};
---
+++ src/reducers/reduceApplicantData.js
@@ -0,0 +1,34 @@
+export const SET_ORG = "SET_ORG";
+export const SET_APPLICANT = "SET_APPLICANT";
+export const SET_CONSENT = "SET_CONSENT";
+
+const reducerLookup = {
+  SET_ORG: (state, action) => {
+    return {
+      ...state,
+      org: action.value
+    };
+  },
+  SET_APPLICANT: (state, action) => {
+    return {
+      ...state,
+      applicant: action.value
+    };
+  },
+  SET_CONSENT: (state, action) => {
+    return {
+      ...state,
+      consent: action.value
+    };
+  }
+};
+
+export default function reducer(state, action) {
+  if (reducerLookup[action.type]) {
+    return reducerLookup[action.type](state, action);
+  }
+
+  throw new Error(
+    `Tried to reduce with unsupported action type: ${action.type}`
+  );
+};
